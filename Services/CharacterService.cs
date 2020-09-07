6144715f36ed81be69b0e100ccb5d0daef586e58
Oleksandrbu <oleksandrbu@gmail.com>
2020-09-07 15:29:46 +0300

    Add Character function

--- Services/CharacterService.cs
+++ Services/CharacterService.cs
@@ -27,5 +27,27 @@ namespace image_browser{
             db.SaveChanges();
             return character;
         }
+        public List<Image> GetImages(long id){
+            db.Images.Load();
+            List<Image> charList = new List<Image>();
+            List<ImageCharacter> imCharList = db.ImageCharacters.Where(p => p.CharacterId == id).ToList();
+            if (imCharList != null){
+                foreach (ImageCharacter character in imCharList){
+                    charList.Add(character.Image);
+                }
+            }   
+            return charList;
+        }   
+        public List<Title> GetTitles(long id){
+            db.Titles.Load();
+            List<Title> charList = new List<Title>();
+            List<TitleCharacter> imCharList = db.TitleCharacters.Where(p => p.CharacterId == id).ToList();
+            if (imCharList != null){
+                foreach (TitleCharacter character in imCharList){
+                    charList.Add(character.Title);
+                }
+            }   
+            return charList;
+        }   
     }
 }
